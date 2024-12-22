ee43c3faac1ef2ad4ec528157242b3789f59f366
chrisramvel <christian.ramosvelez@gmail.com>
2024-12-22 14:54:33 +0100

    Primer commiy

---
+++ Exercise.py
@@ -0,0 +1,10 @@
+numeros = [5, 8, 10, -3, 2, 1]
+
+for numero in numeros:
+    print(numero)
+    if numero < 0:
+        print('El primer número negativo encontrado es:', numero)
+        break
+
+#Como ejercicio/reto, tratar de añadir a la lista un -7, poner un contador antes del bucle for#
+#y en el print sustituir "primer" por "contador", y que me de el resultado de todos los números negativos#
---
+++ Exercise_10.py
@@ -0,0 +1,8 @@
+
+#Modifica el script anterior para que imprima cada letra con un prefijo '---'
+
+hero = 'Batman'
+prefix = '---'
+
+for i in(hero):
+    print(prefix, i)
